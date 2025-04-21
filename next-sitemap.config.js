e76bfc178fc98b80b90e7961f556390c4d3d05fa
Mikhail Anisin <m.anisin@sign.me>
2025-04-21 13:07:11 +0300

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://4-trip.ru", // или другой домен
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  changefreq: "daily",
+  priority: 0.7,
+};
