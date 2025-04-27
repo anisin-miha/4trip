30da156fd9d82d766a4477c59e0f3d60205aaa33
Mikhail Anisin <m.anisin@sign.me>
2025-04-27 14:29:18 +0300

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -24,7 +24,7 @@ let nextConfig = {
   },
   images: {
     loader: "custom",
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    imageSizes: [128, 256, 384],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
   transpilePackages: ["next-image-export-optimizer"],
@@ -58,7 +58,6 @@ let nextConfig = {
 
 mergeConfig(nextConfig, userConfig);
 
-// 👇 применяем PWA-обёртку в самом конце
 nextConfig = withPWAWrapped(nextConfig);
 
 export default nextConfig;
